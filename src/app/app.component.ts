19b28ca0d60199648737947617a63489637679ca
emiliya.chavdarova <emiliya.chavdarova@colleto.de>
2025-04-10 02:11:48 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {TranslateDirective, TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {NgTemplateOutlet, UpperCasePipe} from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-	imports: [RouterOutlet, RouterLink],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	imports: [
+		RouterOutlet,
+		RouterLink,
+		TranslatePipe,
+		UpperCasePipe,
+		NgTemplateOutlet
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'personal';
+	menuItems = ['about', 'courses', 'experience', 'projects'];
+
+	constructor(protected translate: TranslateService) {
+		this.translate.addLangs(['de', 'en']);
+		this.translate.setDefaultLang('en');
+		this.translate.use('en');
+	}
+
+	changeLanguage() {
+		this.translate.use(this.translate.currentLang === 'en' ? 'de':'en')
+	}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,38 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import {provideRouter, withHashLocation, withRouterConfig} from '@angular/router';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter, RouterModule, withHashLocation, withRouterConfig} from '@angular/router';
+import {routes} from './app.routes';
+import {provideTranslateService, TranslateLoader,} from '@ngx-translate/core';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
-import { routes } from './app.routes';
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+	new TranslateHttpLoader(http, 'i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-	  provideZoneChangeDetection({
-		  eventCoalescing: true
-	  }),
-	  provideRouter(
-		  routes,
-		  withHashLocation(),
-		  withRouterConfig({
-			  onSameUrlNavigation: 'reload'
-		  })
-	  )
-  ]
+	providers: [
+		provideHttpClient(),
+		provideZoneChangeDetection({
+			eventCoalescing: true
+		}),
+		provideRouter(
+			routes,
+			withHashLocation(),
+			withRouterConfig({
+				onSameUrlNavigation: 'reload'
+			})
+		),
+		importProvidersFrom(RouterModule.forRoot(routes, {
+			scrollPositionRestoration: 'enabled',  // Enables scroll restoration
+			anchorScrolling: 'enabled'             // Enables anchor scrolling
+		})),
+		provideTranslateService({
+			defaultLanguage: 'en',
+			loader: {
+				provide: TranslateLoader,
+				useFactory: httpLoaderFactory,
+				deps: [HttpClient],
+			},
+		})
+
+	]
 };
